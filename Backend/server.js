469114c35c51945693b1101b02aaf0864d6d07a3
Mukesh Chandra <mukeshjoshi0301@gmail.com>
2024-07-14 19:15:52 +0530

    url updated in cors

--- Backend/server.js
+++ Backend/server.js
@@ -10,10 +10,18 @@ const errorHandler = require('./controllers/errroMiddleware')
 const app = express();
 
 const PORT = process.env.PORT || 5002;
+const corsOptions = {
+  origin: 'https://hindi-brainvoyage.vercel.app/', // Replace with your actual Vercel frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false})); 
 
